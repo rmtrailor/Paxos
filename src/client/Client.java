3bf2c5f3f6f377bb2946bbea887024bbd41d3742
rmtrailor <rmtrailor@dons.usfca.edu>
2017-04-21 22:58:18 -0700

    Add initial version of Paxos files

---
+++ src/client/Client.java
@@ -0,0 +1,8 @@
+package client;
+
+/**
+ * This class acts as a client that will send requests to a Paxos Layer
+ */
+public class Client {
+
+}
---
+++ src/paxos/PaxosLayer.java
@@ -0,0 +1,15 @@
+package paxos;
+
+/**
+ * This class will set up the layer for implementing Paxos and will accept requests
+ */
+public class PaxosLayer {
+
+    /**
+     * Initializes the layer by setting up N nodes where N = numNodes
+     * @param numNodes The number of paxos nodes to create
+     */
+    public PaxosLayer(int numNodes) {
+
+    }
+}
